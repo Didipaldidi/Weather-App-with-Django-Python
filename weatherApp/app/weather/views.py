4d20589a5fef22477db2c1c17f844d812275db3c
Valdi <valdilaurentius@gmail.com>
2023-06-29 21:03:53 -0700

    refactor get_html_content function

--- weatherApp/app/weather/views.py
+++ weatherApp/app/weather/views.py
@@ -6,15 +6,15 @@ from .forms import CityForm, DeleteCityForm
 
 # Create your views here.
 def get_html_content(location_data):
-        USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
-        LANGUAGE = "en-US,en;q=0.5"
-        session = requests.Session()
-        session.headers['User-Agent'] = USER_AGENT
-        session.headers['Accept-Language'] = LANGUAGE
-        session.headers['Content-Language'] = LANGUAGE
-        city = location_data['city']
-        html_content = session.get(f'https://www.google.com/search?q=weather+{city}').text #get the weather information from webscraping
-        return html_content
+    USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36"
+    LANGUAGE = "en-US,en;q=0.5"
+    session = requests.Session()
+    session.headers['User-Agent'] = USER_AGENT
+    session.headers['Accept-Language'] = LANGUAGE
+    session.headers['Content-Language'] = LANGUAGE
+    city = location_data['city']
+    html_content = session.get(f'https://www.google.com/search?q=weather+{city}').text
+    return html_content
 
 def get_current_loc_info():
     ip = requests.get('https://api.ipify.org?format=json')# url for the ip api

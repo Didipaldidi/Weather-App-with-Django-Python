8463271aff478e784b2d1ba9b8a981af202827c7
Valdi <valdilaurentius@gmail.com>
2023-07-18 10:52:37 -0700

    added test cases for forecast app

--- weatherApp/app/forecast/tests.py
+++ weatherApp/app/forecast/tests.py
@@ -1,3 +1,16 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class DetailsViewTestCase(TestCase):
+    def setUp(self):
+        self.city_name = 'New York'
+        self.api_key = 'your_api_key_here'  # Replace with your valid API key
+
+    def test_details_view(self):
+        response = self.client.get(reverse('details', args=[self.city_name]))
+        self.assertEqual(response.status_code, 200)  # Should return 200 for successful request
+        self.assertContains(response, self.city_name)  # Should contain the city name in the response content
+        self.assertContains(response, 'datetime')  # Should contain the forecast datetime
+        self.assertContains(response, 'temperature')  # Should contain the forecast temperature
+        self.assertContains(response, 'weather')  # Should contain the forecast weather
+        self.assertContains(response, 'description')  # Should contain the forecast description
